51b300eb97d7d6f0161317fbc2468b2f4ae4246c
saramabbas <saramabbas2@yahoo.com>
2022-06-21 14:43:31 +0100

    fixed bugs

--- gyro_out.py
+++ gyro_out.py
@@ -128,9 +128,9 @@ for w in range (n_m): #Inclinometer Data-Reading Loop:
 
  average_time = np.mean(time_array, axis=1)
  average_timing_array = np.append(average_timing_array, average_time)
- average_timing_array [0]= round (average_timing_array [0],4) 
+ average_timing_array [0]= round(average_timing_array[0],4) 
  
- print (average_time[0], average_roll_pitch[0], average_roll_pitch[1])
+ print (average_timing_array[0], average_roll_pitch[0], average_roll_pitch[1])
  
 
  # write a row to the csv file
