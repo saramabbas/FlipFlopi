b4c356370cc2bf22344c86573f2b62de99433f34
saramabbas <saramabbas2@yahoo.com>
2022-06-21 14:38:39 +0100

    fixed bugs

--- gyro_out.py
+++ gyro_out.py
@@ -127,7 +127,7 @@ for w in range (n_m): #Inclinometer Data-Reading Loop:
  
 
  average_time = np.mean(time_array, axis=1)
- average_timing_array.append(average_timing_array, average_time)
+ average_timing_array = np.append(average_timing_array, average_time)
  print (average_time, average_roll_pitch[0], average_roll_pitch[1])
  
 
